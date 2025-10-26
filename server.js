4e8f84a897708c92940f7c73c11419ecb06694c7
abu bakar <abubakarkhossa@gmail.com>
2025-10-26 10:21:11 +0500

    change

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import connectDB from './config/db.js';
 import userRoutes from "./routes/user.Routes.js"
-const app=express()
+
+const app = express();
 
 dotenv.config();
-const PORT=process.env.PORT || 5000
-app.use(express.json())
+const PORT = process.env.PORT || 5000;
 
-app.use("/api/users", userRoutes)
+// CORS should be before routes
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 
-app.listen(PORT, ()=>{
-    connectDB();
-    console.log("Server is Connected succsfully")
-})
+app.use(express.json());
+
+// Routes always after CORS
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server is Connected successfully");
+});
