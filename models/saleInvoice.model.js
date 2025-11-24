7f3246b4fae308ed44f2475a432ae150aaf0c425
abubakarkhosa <abakarkhossa@gmail.com>
2025-11-24 19:43:53 +0500

    a

--- models/saleInvoice.model.js
+++ models/saleInvoice.model.js
@@ -1,25 +1,17 @@
 
 import mongoose from "mongoose";
-import Joi from "joi";
 
 const itemSchema = new mongoose.Schema({
-  hsCode: { type: String},
-  productDescription: { type: String},
+  hsCode: { type: String },
+  productDescription: { type: String },
   uoM: { type: String },
-  // taxType: {
-  //   descriptionType: { type: String, required: true },
-  //   salesTaxValue: { type: Number, required: true },
-  // },
   quantity: { type: Number },
-  
-  // price: { type: Number, required: true },
+  price: { type: Number,  },
 
   furtherTax: { type: Number, default: 0 },
-
-  valueSalesExcludingST: { type: Number},
+  valueSalesExcludingST: { type: Number },
   salesTaxWithheldAtSource: { type: Number },
 
-  
   rate: { type: String },
   saleType: { type: String },
   discount: { type: Number, default: 0 },
@@ -27,7 +19,6 @@ const itemSchema = new mongoose.Schema({
   sroItemSerialNo: { type: String, default: "" },
   sroScheduleNo: { type: String, default: "" },
   salesTaxApplicable: { type: Number, default: 0 },
-  // valueSalesExcludingST: { type: Number, default: 0 },
   totalValues: { type: Number, default: 0 },
   fixedNotifiedValueOrRetailPrice: { type: Number, default: 0 },
   extraTax: { type: String, default: "" },
@@ -38,27 +29,26 @@ const salesInvoiceSchema = new mongoose.Schema(
     invoiceType: { type: String, default: "Sale Invoice" },
     invoiceDate: { type: String, required: true },
 
-  
     sellerNTNCNIC: { type: String, required: true },
     sellerBusinessName: { type: String, required: true },
     sellerProvince: { type: String, required: true },
     sellerAddress: { type: String, required: true },
 
-    
     buyerNTNCNIC: { type: String, required: true },
     buyerBusinessName: { type: String, required: true },
     buyerProvince: { type: String, required: true },
     buyerAddress: { type: String, required: true },
     buyerRegistrationType: { type: String, enum: ["Registered", "Unregistered"], required: true },
 
-  
     scenarioId: { type: String, default: "" },
     invoiceRefNo: { type: String, default: "" },
-    FBRToken:{type: String, required: true},
+    FBRToken: { type: String, required: true },
 
     items: [itemSchema],
 
-  
+    fbrResponse: { type: String },
+    fbrResponseDate: { type: Date }, 
+
     grandTotal: { type: Number },
 
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
