1096cd98a30ebfeadcd24e14a4b22a20d3dca6d2
abubakarkhosa <abakarkhossa@gmail.com>
2025-12-08 14:12:28 +0500

    dfhdsfdjfhd

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -24,7 +24,7 @@ export const sendPasswordResetEmail = async (toEmail, resetURL) => {
   const resetLink = `http://localhost:5173/reset-password/${resetURL}`;
   const htmlContent = RESET_PASSWORD_TEMPLATE.replace("{{resetURL}}", resetLink);
   const mailOptions = {
-    from: '"ABU BAKAR SIDDIQUE" <mailtrap@demomailtrap.com>',
+    from: `DevOx Syndicate <${process.env.MAILTRAP_USER}>`,
     to: toEmail,
     subject: "Reset Your Password",
     html: htmlContent,
@@ -36,4 +36,23 @@ export const sendPasswordResetEmail = async (toEmail, resetURL) => {
   } catch (error) {
     console.error("❌ Error sending email:", error);
   }
+};
+
+export const sendForgotPassEmail = async (email, resetToken) => {
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: process.env.MAILTRAP_USER,
+      pass: process.env.MAILTRAP_PASS,
+    },
+  });
+  const mailOptions = {
+    from: `DevOx Syndicate  <${process.env.MAILTRAP_USER}>`,
+    to: email,
+    subject: "Password Reset",
+
+    html: generateForgotEmailTemplate(resetToken),
+  };
+
+  return transporter.sendMail(mailOptions);
 };
